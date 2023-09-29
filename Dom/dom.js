5116d2e832c8cf4b9448d373d5600aa59dc65aa5
rence22 <142190116+rence22@users.noreply.github.com>
2023-09-29 21:01:26 +0800

    Update dom.js

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,1 +1,16 @@
+let txtCat = document.getElementById("txtCat");
+let btnAdd = document.getElementById("btnAdd");
+let ulCatList = document.getElementById("ulCatList");
+
+btnAdd.addEventListener("click", updateCatList);
+
+function updateCatList() {
+    let newCategory = txtCat.value;
+    if (newCategory.trim() !== "") {
+        let li = document.createElement("li");
+        li.textContent = newCategory;
+        ulCatList.appendChild(li);
+        txtCat.value = "";
+    }
+}
 
